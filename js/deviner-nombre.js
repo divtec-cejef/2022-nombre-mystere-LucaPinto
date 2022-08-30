63dc6da3bb08947df3292b54d1359b57a2987792
Luca Pinto <94682198+LucaPinto@users.noreply.github.com>
2022-08-30 11:04:15 +0200

    exercice terminé

--- js/deviner-nombre.js
+++ js/deviner-nombre.js
@@ -1,13 +1,18 @@
 /**
 * Jeu du nombre mystère
-* @author  Steve Fallet
+* @author  Luca Pinto
 * @version 0.1
-* @since   2018-09-09 (date de création)
+* @since   2022-08-30 (date de création)
 */
 
 // Main IIFE (Immediately-Invoked Function Expression, se prononce "iffy")
 (function main() {
-    "use strict";
+    'use strict';
+
+    let nbMystere = 0;
+    let nbEssais = 0;
+    let reponse = 0;
+    let message = " ";
 
     /**
      * Retourne un nombre entier aléatoire compris entre min et max
@@ -17,6 +22,21 @@
      */
     function tireNombre(min, max) {
         return Math.floor(Math.random() * (max - min) + min);
+    }
+
+    nbMystere = tireNombre(1, 50);
+
+    while (true) {
+        reponse = Number(prompt('Entrez un nombre entre 1 et 100'));
+        nbEssais++;
+        if (reponse === nbMystere) {
+            alert('Vous avez trouvez apres ' + nbEssais + ' essais');
+            return;
+        }else if (reponse > nbMystere) {
+            alert('Cest moins !');
+        }else {
+            alert('Cest plus !');
+        }
     }
 
 }()); // main IIFE
